6e4b255bd895858ed5ec620be27738b363e29e83
Diego Araújo <diegoaraujopro@gmail.com>
2022-10-09 17:11:55 -0300

    Adiciona as soluções dos desafios do módulo 2

---
+++ 02.09. Desafio - variáveis e operadores aritméticos/Comercio.java
@@ -0,0 +1,14 @@
+public class Comercio {
+
+  public static void main (String [] args) {
+  
+  int ticket1 = 20; 
+  int ticket2 = 30;
+  int ticket3 = 100;
+  
+  int mediaTicket = (ticket1 + ticket2 + ticket3) /3;
+  
+  System.out.println("A média dos ticket é: " + mediaTicket );
+  
+  }
+}
---
+++ 02.23. Desafio - String, entrada de dados, printf, etc/CalculaPagamento.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CalculaPagamento {
+
+  public static void main (String [] args) {
+  
+  // Programa para calcular a folha de pagamento
+  
+  Scanner entrada = new Scanner(System.in);
+  
+  System.out.print("Digite o nome do funcionário: ");
+  String nome = entrada.nextLine();
+  
+  System.out.print("Digite o valor da hora trabalhada: ");
+  double valorHora = entrada.nextDouble();
+  
+  System.out.print("Digite a quantidade de horas trabalhadas: "); 
+  int horasTrabalhadas = entrada.nextInt();
+
+  System.out.print("Digite o valor dos descontos: "); 
+  double valorDesconto = entrada.nextDouble();
+    
+  System.out.println("-------------------------");
+  System.out.printf("Folha de pagamento de %s%n", nome);
+  System.out.printf("%dh X R$%7.2f = R$%8.2f%n",horasTrabalhadas, valorHora,  horasTrabalhadas * valorHora);
+  System.out.printf("Descontos: R$%7.2f%n", valorDesconto);
+  System.out.printf("Total líquido: R$%8.2f%n", (horasTrabalhadas * valorHora) - valorDesconto);
+    
+  
+  
+  }
+}
