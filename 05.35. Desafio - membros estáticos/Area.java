75ce2fd593fb189af9a01735281e362c221cfdaa
Diego Araújo <diegoaraujopro@gmail.com>
2023-05-08 20:37:40 -0300

    Adiciona solução do desafio 05.35 - membros estático

---
+++ 05.35. Desafio - membros estáticos/Area.java
@@ -0,0 +1,10 @@
+public class Area {
+
+    static double calcularAreaQuadrado(double lado) {
+        return lado * lado;
+    }
+
+    static double calcularAreaCirculo(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ 05.35. Desafio - membros estáticos/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Área do quadrado: " + Area.calcularAreaQuadrado(20.0));
+        System.out.println("Área do círculo: " + Area.calcularAreaCirculo(50.0));
+    }
+}
